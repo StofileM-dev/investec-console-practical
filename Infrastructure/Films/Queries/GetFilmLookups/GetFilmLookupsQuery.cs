927e3af1235fadcbce94fd2f3a1e5897e159f381
StofileM-dev <56542807+StofileM-dev@users.noreply.github.com>
2025-11-18 21:16:07 +0200

    Add Infrastructure Layer

---
+++ Infrastructure/Films/Queries/GetFilmLookups/GetFilmLookupsQuery.cs
@@ -0,0 +1,50 @@
+﻿using Domain.Entities;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Infrastructure.Films.Queries.GetFilmLookups;
+
+public class GetFilmLookupsQuery
+{
+    private readonly HttpClient _http;
+
+    public GetFilmLookupsQuery(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<Dictionary<string, string>> GetFilmLookupAsync(IEnumerable<Person> people)
+    {
+        var filmLookup = new Dictionary<string, string>();
+
+        var filmUrls = people
+            .SelectMany(p => p.films)
+            .Distinct()
+            .ToList();
+
+        foreach (var filmUrl in filmUrls)
+        {
+            var response = await _http.GetAsync(filmUrl);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.Error.WriteLine($"Request error on {filmUrl}");
+                continue;
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var filmJson = JsonConvert.DeserializeObject<JObject>(content);
+
+            var title = filmJson?["title"]?.ToString() ?? "Unknown";
+
+            filmLookup[filmUrl] = title;
+        }
+
+        if (!filmLookup.Any())
+        {
+            throw new Exception("Film lookup do not exist found!");
+        }
+
+        return filmLookup;
+    }
+}
---
+++ Infrastructure/People/Queries/GetAllPeople/GetAllPeopleQuery.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Entities;
+using Newtonsoft.Json;
+
+namespace Infrastructure.People.Queries.GetAllPeople;
+
+public class GetAllPeopleQuery
+{
+    private readonly HttpClient _http;
+
+    public GetAllPeopleQuery(HttpClient http)
+    {
+        _http = http;
+    }
+
+    public async Task<List<Person>> GetAllPeopleAsync()
+    {
+        var people = new List<Person>();
+        var pageLink = "https://swapi.dev/api/people";
+
+        do
+        {
+            var response = await _http.GetAsync(pageLink);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.Error.WriteLine($"Request error on {pageLink}");
+                break;
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject<Result>(content);
+
+            if (result?.results == null || !result.results.Any())
+            {
+                break;
+            }
+
+            people.AddRange(result.results);
+            pageLink = result.next;
+        } while (!string.IsNullOrEmpty(pageLink));
+
+        if (!people.Any())
+        {
+            throw new Exception("Not people found!");
+        }
+
+        return people;
+    }
+}
